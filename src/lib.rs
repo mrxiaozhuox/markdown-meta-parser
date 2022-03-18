2941b22022ccbd79a28f02efbf9876a9055ec457
mrxiaozhuox <mrxzx@wwsg18.top>
2022-03-18 12:05:44 +0800

    feat: commit code

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,104 @@
 use std::collections::HashMap;
 
-use nom::{bytes::complete::take_till1, IResult};
-use serde_json::{Map, Value};
-
 #[derive(Debug, Clone)]
 pub struct MetaData {
-    result: Value,
     content: String,
-    config: MetaDataConfig,
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct MetaDataConfig {
     pub required: Vec<String>,
-    pub type_mark: HashMap<String, TypeMark>,
+    pub type_mark: HashMap<String, String>,
 }
 
 #[derive(Debug, Clone)]
-pub enum TypeMark {
-    String,
-    Number,
-    Array,
-    Bool,
+pub enum Value {
+    String(String),
+    Number(f64),
+    Array(Vec<String>),
+    Bool(bool),
 }
 
 impl MetaData {
-    pub fn new(content: &str, config: MetaDataConfig) -> Self {
+    pub fn new(content: &str) -> Self {
         Self {
-            result: Value::Object(Map::new()),
             content: content.to_string(),
-            config,
+            required: vec![],
+            type_mark: Default::default(),
         }
     }
 
-    pub fn parse_line(text: &str) -> IResult<&str, (&str, &str)> {
-        let r = take_till1(|c| c == ':')(text)?;
+    pub fn parse(&self) -> anyhow::Result<HashMap<String, Value>> {
+        let content = self.content.clone();
+        let mut result = HashMap::new();
 
-        let key = r.1.trim();
-        let value = r.0[1..].trim();
+        let mut started = false;
 
-        // empty str, (key, value)
-        Ok(("", (key, value)))
-    }
-}
+        for line in content.lines() {
+            // if line is empty, just skip
+            if line.trim().is_empty() {
+                continue;
+            }
 
-impl Default for MetaData {
-    fn default() -> Self {
-        Self {
-            result: Value::Null,
-            content: Default::default(),
-            config: Default::default(),
+            if line.trim() == "---" {
+                started = !started;
+            }
+
+            if started {
+                let res = Self::parse_line(line);
+                println!("{:?}", res);
+            }
         }
+
+        for req in &self.required {
+            if !result.contains_key(req) {
+                return Err(anyhow::anyhow!("Required key not found: {req}"));
+            }
+        }
+
+        Ok(result)
+    }
+
+    pub fn parse_line(text: &str) -> Option<(String, String)> {
+        let mut v = text.split(':').collect::<Vec<&str>>();
+        let mut result = (String::new(), String::new());
+        if v.len() < 2 {
+            return None;
+        }
+
+        result.0 = v.get(0).unwrap().trim().into();
+        v.remove(0);
+        result.1 = v.join(":").trim().into();
+
+        Some(result)
     }
 }
 
 #[cfg(test)]
 mod test {
+    use std::collections::HashMap;
+
     use crate::MetaData;
 
     #[test]
     fn one_line() {
-        let _meta = MetaData::default();
-        let r = MetaData::parse_line("title : Hello World");
+        let r = MetaData::parse_line("title: Hello World");
         println!("{:?}", r);
+    }
+
+    #[test]
+    fn test_md() {
+        const md: &str = "
+---
+title: Hello World
+author: YuKun Liu
+tags: [test]
+date: 2003-11-10
+---
+        ";
+
+        let meta = MetaData {
+            content: md.to_string(),
+            required: vec![],
+            type_mark: HashMap::default(),
+        };
+
+        let r = meta.parse();
     }
 }
